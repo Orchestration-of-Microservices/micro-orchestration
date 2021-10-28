d3d6c26d47cfee42810c62ba10f966ba1a3f0706
Roman <mister02.rv@gmail.com>
2021-10-28 11:54:32 +0300

    add test

--- lab2/services/service2/src/app.js
+++ lab2/services/service2/src/app.js
@@ -2,7 +2,17 @@ const express = require('express')
 
 const app = express()
 
+let status = 'ok'
+
+app.get('/api/service2/untested-request', async (req, res, next) => {
+    status = 'failed'
+    res.status(200).send('Service successfully broken').end()
+})
+
 app.get('/api/service2', async (req, res, next) => {
+    if (status != "ok") {
+        setTimeout(() => {}, 10 * 1000)
+    }
     res.status(200).send('Hello from second node server').end()
 })
 
