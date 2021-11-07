f05b498b9e6cb0113b0b6b2204543594eefced5a
Roman <mister02.rv@gmail.com>
2021-11-07 15:42:56 +0200

    wip

--- lab3/services/service1/src/app.js
+++ lab3/services/service1/src/app.js
@@ -1,9 +1,18 @@
+require('dotenv').config();
+
 const express = require('express')
+const bodyParser = require('body-parser');
 
 const app = express()
 
+require('./utilities/migrations');
+require('./models');
+
 require('./consumers/mobile-consumer')
 require('./consumers/notification-consumer')
+
+app.use(bodyParser.urlencoded({ extended: true, strict: false }));
+app.use(bodyParser.json());
 
 app.get('/api/service1', async (req, res, next) => {
     res.status(200).send('Hello from first node server').end()
