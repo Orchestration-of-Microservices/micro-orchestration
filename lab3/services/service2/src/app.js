f6010438958aad2f63ddeb9ed5f34fd88bfa9aee
Roman <mister02.rv@gmail.com>
2021-11-07 13:01:58 +0200

    add producer

--- lab3/services/service2/src/app.js
+++ lab3/services/service2/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const producer = require('./producer/index')
 
 const app = express()
 
@@ -18,6 +19,19 @@ app.get('/api/service2', async (req, res, next) => {
     res.status(200).send('Hello from second node server').end()
 })
 
+app.post('/api/service2/message', async (req, res, next) => {
+    const notificationMessage = req.body.notificationMessage
+    const mobileMessage = req.body.mobileMessage
+
+    try {
+        producer.produce('notification', null, Buffer.from(notificationMessage), 'Stormwind', Date.now());
+        producer.produce('mobile', null, Buffer.from(mobileMessage), 'Stormwind', Date.now());
+    } catch (err) {
+        console.error('A problem occurred when sending our message');
+        console.error(err);
+    }
+
+    res.status(200).send('Message send').end()
+})
+
 module.exports = app
-
-
---
+++ lab3/services/service2/src/producer/index.js
@@ -0,0 +1,19 @@
+const producer = new Kafka.Producer({
+    'metadata.broker.list': 'localhost:9092',
+    'dr_cb': true
+});
+
+producer.connect();
+
+producer.on('ready', function() {
+    console.log('Kafra producer ready to produce messages...')
+});
+
+producer.on('event.error', function(err) {
+    console.error('Error from producer');
+    console.error(err);
+})
+
+producer.setPollInterval(100);
+
+module.exports = producer
