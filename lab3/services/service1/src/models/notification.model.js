d3247ab89a607779ae49b6673236c775f5069952
Roman <mister02.rv@gmail.com>
2021-11-08 23:38:43 +0200

    add migrations

--- lab3/services/service1/src/models/notification.model.js
+++ lab3/services/service1/src/models/notification.model.js
@@ -7,13 +7,7 @@ module.exports = function (sequelize, DataTypes) {
         message: {
             type: DataTypes.TEXT,
             allowNull: true
-        },
-        createdAt: {
-            field: 'created_at',
-            allowNull: false,
-            defaultValue: Sequelize.fn('NOW'),
-            type: Sequelize.DATE
-        },
+        }
     }, {
         updatedAt: false,
         underscored: true,
