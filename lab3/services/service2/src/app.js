3145984633316b58e555782b962fd765895fceda
Roman <mister02.rv@gmail.com>
2021-11-09 21:30:08 +0200

    fix partition

--- lab3/services/service2/src/app.js
+++ lab3/services/service2/src/app.js
@@ -31,7 +31,7 @@ app.post('/api/service2/message', async (req, res, next) => {
     const notificationMessage = req.body.notification
     const mobileMessage = req.body.mobile
 
-    const partition = notificationMessage[0] < "N" ? 0 : 1;
+    const partition = notificationMessage.to[0] < "N" ? 0 : 1;
 
     try {
         await producer.send({
